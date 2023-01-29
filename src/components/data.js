9942c69b36a4da9b5ce0e4a6aeee7d3ea7a9e983
Aviv Nataf <118442152+an1604@users.noreply.github.com>
2023-01-29 14:48:36 +0200

    Add files via upload

---
+++ src/components/data.js
@@ -0,0 +1,26 @@
+export default[
+    {
+        id:1,
+        name:'mark zuckerberg' , 
+        age: 22,
+        image:"https://th.bing.com/th/id/OIP.uxNrZH2MDoTpEWTrimPZRwHaE7?pid=ImgDet&rs=1"
+    },
+    {
+        id:2,
+        name:'lebron james',
+        age: 38,
+        image:"https://th.bing.com/th/id/R.ad10d6022385b5029d112d44a927cd7f?rik=%2fjvPT%2bVeohluFA&pid=ImgRaw&r=0"
+    },
+    {
+        id:3,
+        name:'travis scott',
+        age: 29,
+        image:"https://omgeee.com/wp-content/uploads/skynews-travis-scott-astroworld_5573660.jpg"
+    },
+    {
+        id:4,
+        name:'Aviv Nataf',
+        age: 22,
+        image:"https://media.licdn.com/dms/image/D4D03AQHfEZiG1kF2Mg/profile-displayphoto-shrink_800_800/0/1668264931290?e=1680739200&v=beta&t=DDyTjLAIf0DeIZTBlrTQCeaaxyczxDylAxe1iqXeg5M"
+    }
+]
